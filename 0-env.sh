de4355be6dcf6dd201a4113b95239960acce2429
Toe Zju <zjutoe@gmail.com>
2016-04-07 22:47:04 +0800

    refinement

--- 0-env.sh
+++ 0-env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#yum install -y redis
-#yum install -y mysql-server
+yum install -y redis
+yum install -y mysql-server
 
-docker run --name of-redis -p 6379:6379 -d redis
-docker run --name of-mysql -e MYSQL_ROOT_PASSWORD='' -e MYSQL_ALLOW_EMPTY_PASSWORD=1 -p 3306:3306 -d mysql
+#docker run --name of-redis -p 6379:6379 -d redis
+#docker run --name of-mysql -e MYSQL_ROOT_PASSWORD='' -e MYSQL_ALLOW_EMPTY_PASSWORD=1 -p 3306:3306 -d mysql
--- 6-install-components.sh
+++ 6-install-components.sh
@@ -6,6 +6,7 @@ cd ~
 cp `find workspace/src/github.com/ -name "falcon-*.tar.gz"` $TARGET/tmp/
 cp workspace/src/github.com/open-falcon/query/graph_backends.txt $TARGET/query
 cp -a workspace/src/github.com/open-falcon/dashboard $TARGET/
+cp control $TARGET
 
 cd $TARGET
 
@@ -16,4 +17,4 @@ for x in `find ./tmp/ -name "*.tar.gz"`;do
     cp $app/cfg.example.json $app/cfg.json     
 done
 
-cp control $TARGET
+
