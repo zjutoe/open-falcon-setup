c18426e27957e8d46427b82aea2c8e4b659b9705
Toe Zju <zjutoe@gmail.com>
2016-04-08 00:19:08 +0800

    update mysql docker setup

--- 0-env.sh
+++ 0-env.sh
@@ -4,4 +4,4 @@
 #yum install -y mysql-server
 
 docker run --name of-redis -p 6379:6379 -d redis
-docker run --name of-mysql -e MYSQL_ROOT_PASSWORD='' -e MYSQL_ALLOW_EMPTY_PASSWORD=1 -p 3306:3306 -d mysql
+docker run --name of-mysql --restart always -d --volumes-from=wifi-data-mysql -e MYSQL_ROOT_PASSWORD='' -e MYSQL_ALLOW_EMPTY_PASSWORD=1 -p 3306:3306 -d mysql
